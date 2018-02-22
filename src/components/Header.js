c809fb2c51bc04ec9c37d8d1afc8148bfcfa8ae2
Mariusz Saramak <maropce@gmail.com>
2018-02-22 14:36:44 +0100

    define textStyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 // import libraries for making a component
 import React, { Component } from 'react'
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 // Make a component
 export default class Header extends Component {
+
   render() {
-    return (<Text>Albums!</Text>);
+    return (<Text style={styles.textStyle}>Albums!</Text>);
   }
 }
+const styles = StyleSheet.create({
+    textStyle: {
+      fontSize: 20
+    }
+});
