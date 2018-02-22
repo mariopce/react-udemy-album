414b1c00255b9b2e2179c531c5cf5cc504a71a21
Mariusz Saramak <maropce@gmail.com>
2018-02-22 14:44:42 +0100

    use const style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 // import libraries for making a component
 import React, { Component } from 'react'
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 // Make a component
 export default class Header extends Component {
 
   render() {
-    return (<Text style={styles.textStyle}>Albums!</Text>);
+    const { textStyle, viewStyle } = styles;
+    return (
+      <View style={viewStyle}>
+        <Text style={textStyle}>Albums!</Text>
+      </View>
+    );
   }
 }
 const styles = StyleSheet.create({
+    viewStyle: {
+      backgroundColor: '#f8f8f8'
+    },
     textStyle: {
       fontSize: 20
     }
